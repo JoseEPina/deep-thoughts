495de984768f162923cd4b6cf3e6588b08f15697
JoseEPina <jose.edpina@gmail.com>
2021-12-21 23:32:34 -0700

    Added the utils Auth file. Updated Sign Up and Login components to navigate based on user logged status. Updated App.js to process authentication.

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import NoMatch from './pages/NoMatch';
 import SingleThought from './pages/SingleThought';
 import Profile from './pages/Profile';
 import Signup from './pages/Signup';
+import { setContext } from '@apollo/client/link/context';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
@@ -18,8 +19,30 @@ const httpLink = createHttpLink({
    uri: '/graphql',
 });
 
+//* With the configuration of authLink, we use the setContext() function to retrieve the token from localStorage and
+//* set the HTTP request headers of every request to include the token, whether the request needs it or not. This is
+//* fine, because if the request doesn't need the token, our server-side resolver function won't check for it.
+//*
+//* We wont use the first parameter of setContext(), which stores the current request object in case this function
+//* is running after we've initiated a request. Because we're not using the first parameter, but we still need to
+//* access the second one, we can use an underscore _ to serve as a placeholder for the first parameter.
+const authLink = setContext((_, { headers }) => {
+   const token = localStorage.getItem('id_token');
+   return {
+      headers: {
+         ...headers,
+         authorization: token ? `Bearer ${token}` : '',
+      },
+   };
+});
+
+//* use ApolloClient() constructor to instantiate the Apollo Client and create the connection to the API endpoint
+//*
+//* link: authLink.concat(httpLink) - combine the authLink and httpLink objects so that every request retrieves the
+//* token and sets the request headers before making the request to the API. With this in place, we
+//* won't have to worry about doing this manually with every single request. It'll just do it for us.
 const client = new ApolloClient({
-   link: httpLink,
+   link: authLink.concat(httpLink),
    cache: new InMemoryCache(),
 });
 
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
-import { ADD_USER } from '../utils/mutations';
+import {ADD_USER} from '../utils/mutations';
+import Auth from '../utils/auth';
 
 const Signup = () => {
    const [formState, setFormState] = useState({ username: '', email: '', password: '' });
@@ -27,7 +28,7 @@ const Signup = () => {
          const { data } = await addUser({
             variables: { ...formState },
          });
-         console.log(data);
+         Auth.login(data.addUser.token); //* set token to localStorage and reload page to homepage
       } catch (e) {
          console.error(e);
       }
